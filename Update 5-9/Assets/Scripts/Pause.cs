2e644a8a4cd8ef14dbb4482785ffee3af34faa4d
Kymani1001 <44448443+Kymani1001@users.noreply.github.com>
2019-04-24 02:11:48 -0400

    Add files via upload

---
+++ Update 5-9/Assets/Scripts/Pause.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject PauseScreen, ResumeButton, RetryButton, ReturnButton, PauseButton;
+    
+    
+    public void Paused()
+    {
+        PauseScreen.SetActive(true);
+        PauseButton.SetActive(false);
+        Time.timeScale = 0;
+    }
+
+    
+    public void Resume()
+    {
+        PauseScreen.SetActive(false);
+        PauseButton.SetActive(true);
+       Time.timeScale = 1;
+    }
+
+    public void Retry()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Level");
+    }
+
+    public void Return()
+    {
+        SceneManager.LoadScene("Start Menu");
+    }
+}
