d6c2564e6a2479f723a17d845d9d1f7ec8aa2af2
Kymani1001 <44448443+Kymani1001@users.noreply.github.com>
2019-09-24 01:41:31 -0400

    Add files via upload

---
+++ Midstone Update 1/LevelSelect.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelSelect : MonoBehaviour
+{
+    public Button tutorialButton;
+    public Button level1Button;
+    public Button backButton;
+    public string SceneName;
+
+    public void Level1()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Level");
+    }
+
+    public void Tutorial()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void Back()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Start Menu");
+    }
+}
---
+++ Midstone Update 1/Player.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    //Movements
+    public float speed;
+    public float jump = 10;
+    private const int maxjump = 2;
+    private int currentjump = 0;
+    private bool grounded = true;
+
+    //Timer
+    private float startTime;
+    private bool Finished = false;
+    
+    private Rigidbody rb;
+
+    public float gravity = 9.81f;
+    public ForceMode forceMode;
+
+    //Sounds
+    private int count;
+    public AudioSource Jump;
+    public AudioSource Item;
+    public AudioSource Victory;
+
+    //Buttons
+    public Button restartButton;
+    public Button returnButton;
+    public Button levelButton;
+    public string SceneName;
+
+    //Texts
+    public Text counterText;
+    public Text clearText;
+    public Text timerText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCounterText();
+        clearText.text = "";
+        restartButton.gameObject.SetActive(false);
+        returnButton.gameObject.SetActive(false);
+        levelButton.gameObject.SetActive(false);
+
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //movement
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+
+        rb.AddForce(movement * speed);
+
+        //jumping
+        rb.AddForce(Vector3.down * gravity, forceMode);
+        if (Input.GetKeyDown("space") && (grounded || maxjump > currentjump))
+        {
+            rb.AddForce(Vector3.up * jump, ForceMode.Impulse);
+            grounded = false;
+            currentjump++;
+            Jump.Play();
+        }
+
+        //timer
+        if (Finished)
+            return;
+
+        float t = Time.time - startTime;
+
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f2");
+        timerText.text = minutes + ":" + seconds;
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        grounded = true;
+        currentjump = 0;
+    }
+
+    //Materials
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Collectable"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCounterText();
+            Item.Play();
+        }
+    }
+
+    //Buttons
+    void SetCounterText()
+    {
+        counterText.text = "Materials: " + count.ToString();
+        if(count >= 10)
+        {
+            clearText.text = "Stage Cleared!";
+            restartButton.gameObject.SetActive(true);
+            returnButton.gameObject.SetActive(true);
+            levelButton.gameObject.SetActive(true);
+            timerText.color = Color.magenta;
+            Finished = true;
+            Victory.Play();
+            
+        }
+    }
+
+    public void RetryGame()
+    {
+        SetCounterText();
+        if (count >= 10)
+        {
+            SceneManager.LoadScene(SceneName);
+        }
+    }
+
+    public void ReturnGame()
+    {
+        SetCounterText();
+        if (count >= 10)
+        {
+            SceneManager.LoadScene("Start Menu");
+        }
+           
+    }
+
+    public void LevelSelect()
+    {
+        SetCounterText();
+        if (count >= 10)
+        {
+            SceneManager.LoadScene("Level Select");
+        }
+
+    }
+}
