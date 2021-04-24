1ac994594b0de331b8e78077e278f86bba53fc47
Kymani1001 <Merriott416@icloud.com>
2021-04-24 18:03:50 -0400

    Added a playable version and everything in one folder, separate from updates

---
+++ Cap Stone 2.0/Assets/Scripts/Main_Menu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Main_Menu : MonoBehaviour
+{
+
+    public Button startButton;
+    public Button exitButton;
+    public string SceneName;
+    
+    
+    public void StartGame()
+    {
+        Time.timeScale = 1;
+        //SceneManager.LoadScene(SceneName);
+        SceneManager.LoadScene("Level Select");
+    }
+
+
+    public void ExitGame()
+    {
+        Debug.Log("Game Closed");
+        Application.Quit();
+    }
+}
---
+++ Cap Stone 2.0/Assets/Scripts/Pause.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject PauseScreen, ResumeButton, RetryButton, ReturnButton, PauseButton, LevelButton;
+    public string SceneName;
+    
+    
+    public void Paused()
+    {
+        PauseScreen.SetActive(true);
+        PauseButton.SetActive(false);
+        Time.timeScale = 0;
+    }
+
+    
+    public void Resume()
+    {
+        PauseScreen.SetActive(false);
+        PauseButton.SetActive(true);
+       Time.timeScale = 1;
+    }
+
+    public void Retry()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneName);
+    }
+
+    public void Return()
+    {
+        SceneManager.LoadScene("Start Menu");
+    }
+
+    public void Level()
+    {
+        SceneManager.LoadScene("Level Select");
+    }
+}
